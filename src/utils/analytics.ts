653a330f09807f127cd5fd72679081c1bcbc354b
aon <21188659+aon@users.noreply.github.com>
2023-02-09 21:52:36 -0300

    Remove classnames and update icons and navbar

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,12 +1,10 @@
-export const trackGithubSocial = () => {
-  trackEvent("github_opened");
-};
-export const trackTwitterSocial = () => {
-  trackEvent("twitter_opened");
-};
-export const trackLinkedinSocial = () => {
-  trackEvent("linkedin_opened");
-};
+export const trackEmailSocial = () => trackEvent("email_opened");
+
+export const trackGithubSocial = () => trackEvent("github_opened");
+
+export const trackTwitterSocial = () => trackEvent("twitter_opened");
+
+export const trackLinkedinSocial = () => trackEvent("linkedin_opened");
 
 export const trackEvent = (event: string) => {
   fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}:${event}`, {
