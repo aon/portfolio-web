4b52fe80f11803863f83a08958d293764762ffbd
aon <21188659+aon@users.noreply.github.com>
2023-02-27 01:35:40 -0300

    Fix analytics events paths

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -4,7 +4,7 @@ export const trackTwitterSocial = () => trackEvent("twitter_opened");
 export const trackLinkedinSocial = () => trackEvent("linkedin_opened");
 
 export const trackEvent = (event: string) => {
-  fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}:${event}`, {
+  fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}/:${event}`, {
     mode: "no-cors",
   });
 };
